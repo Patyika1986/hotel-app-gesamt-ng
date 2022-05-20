83291b45e9624226bcac64c0cf0b785627e1febf
Peter Lakatos <peter.lakatos@outlook.de>
2022-05-20 12:59:37 +0200

    Bookings Erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+// hier ist die verbindung zwischen die datein.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Das title ist ein Propeti
 export class AppComponent {
-  title = 'hotel-app';
+  title = 'Hotel Buchungen';
 }
