ab3d0ed1291c419a513519102733272d3e9e759e
Peter Lakatos <peter.lakatos@outlook.de>
2022-05-21 13:33:03 +0200

    create booking component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BookingsComponent } from './bookings/bookings.component';
-import { AppRoutingModule } from './app-routing.module';// das wurde hier installiert wegen das terminal befehl --module=app
+import { AppRoutingModule } from './app-routing.module';
+import { CreateBookingComponent } from './create-booking/create-booking.component';// das wurde hier installiert wegen das terminal befehl --module=app
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookingsComponent
+    BookingsComponent,
+    CreateBookingComponent
   ],
   imports: [
     BrowserModule,
