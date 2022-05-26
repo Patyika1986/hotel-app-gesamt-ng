f5dc4526579a3bc22a697837ed3c69d9910acd96
Peter Lakatos <peter.lakatos@outlook.de>
2022-05-26 10:49:45 +0200

    InMemory Web-APi

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -15,9 +15,7 @@ export class BookingService {
     // element mit der button löschen
     deleteBooking(booking: Booking): void{
       let index = Bookings.indexOf(booking);
-      Bookings.splice(index,1);// splice löscht das eine element wo du cklickst
-      console.log(index);
-  
+      Bookings.splice(index,1);// splice löscht das eine element wo du cklickst  
     }
 
     getBookingById(id: number) : Booking {
