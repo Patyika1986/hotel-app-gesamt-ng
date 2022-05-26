ae281455362a57bd2f2c35cae556d49a7d17a631
Peter Lakatos <peter.lakatos@outlook.de>
2022-05-26 11:02:35 +0200

    HtClientModule eingerichtet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { BookingsComponent } from './bookings/bookings.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CreateBookingComponent } from './create-booking/create-booking.component';// das wurde hier installiert wegen das terminal befehl --module=app
-
 import { FormsModule } from '@angular/forms'; // ist für die value von form value senden und empfangen an component
+import { HttpClientModule } from '@angular/common/http'; // damit http unterstüzt wird.
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +16,8 @@ import { FormsModule } from '@angular/forms'; // ist für die value von form val
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
